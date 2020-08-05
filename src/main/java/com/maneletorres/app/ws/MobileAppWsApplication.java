03e17a95c54363d1db31ca81a2546446525789db
maneletorres <maneletorres@hotmail.com>
2020-08-05 12:58:58 +0200

    HTTP PUT and HTTP DELETE requests handled.

--- src/main/java/com/maneletorres/app/ws/MobileAppWsApplication.java
+++ src/main/java/com/maneletorres/app/ws/MobileAppWsApplication.java
@@ -1,9 +1,13 @@
 package com.maneletorres.app.ws;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
 public class MobileAppWsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/maneletorres/app/ws/ui/controller/UserController.java
+++ src/main/java/com/maneletorres/app/ws/ui/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.maneletorres.app.ws.ui.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,12 +19,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.maneletorres.app.ws.ui.model.request.UpdateUserDetailsRequestModel;
 import com.maneletorres.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.maneletorres.app.ws.ui.model.response.UserRest;
 
 @RestController
 @RequestMapping("/users") // http://localhost:8080/users/
 public class UserController {
+
+	Map<String, UserRest> users;
 
 	@GetMapping
 	public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
@@ -32,12 +38,10 @@ public class UserController {
 
 	@GetMapping(path = "/{userId}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
-		UserRest returnValue = new UserRest();
-		returnValue.setEmail("test@test.com");
-		returnValue.setFirstName("Sergey");
-		returnValue.setLastName("Kargopolov");
-
-		return new ResponseEntity<>(returnValue, HttpStatus.OK);
+		if (users.containsKey(userId))
+			return new ResponseEntity<>(users.get(userId), HttpStatus.OK);
+		else
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 
 	@PostMapping(consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
@@ -48,16 +52,33 @@ public class UserController {
 		returnValue.setFirstName(userDetails.getFirstName());
 		returnValue.setLastName(userDetails.getLastName());
 
+		String userId = UUID.randomUUID().toString();
+		returnValue.setUserId(userId);
+
+		if (users == null)
+			users = new HashMap<>();
+		users.put(userId, returnValue);
+
 		return new ResponseEntity<>(returnValue, HttpStatus.OK);
 	}
 
-	@PutMapping
-	public String updateUser() {
-		return "update user was called";
+	@PutMapping(path = "/{userId}", consumes = { MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_JSON_VALUE }, produces = { MediaType.APPLICATION_XML_VALUE,
+					MediaType.APPLICATION_JSON_VALUE })
+	public UserRest updateUser(@PathVariable String userId,
+			@Valid @RequestBody UpdateUserDetailsRequestModel userDetails) {
+		UserRest storedUserDetails = users.get(userId);
+		storedUserDetails.setFirstName(userDetails.getFirstName());
+		storedUserDetails.setLastName(userDetails.getLastName());
+
+		users.put(userId, storedUserDetails);
+
+		return storedUserDetails;
 	}
 
-	@DeleteMapping
-	public String deleteUser() {
-		return "delete user was called";
+	@DeleteMapping(path = "/{userId}")
+	public ResponseEntity<Void> deleteUser(@PathVariable String userId) {
+		users.remove(userId);
+		return ResponseEntity.noContent().build();
 	}
 }
