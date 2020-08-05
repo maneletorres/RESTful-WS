12da51ba92adedbbd35923d5cd3e2d87c1f0fa79
maneletorres <maneletorres@hotmail.com>
2020-08-05 11:43:38 +0200

    HTTP POST requests handled.

--- src/main/java/com/maneletorres/app/ws/ui/controller/UserController.java
+++ src/main/java/com/maneletorres/app/ws/ui/controller/UserController.java
@@ -1,17 +1,22 @@
 package com.maneletorres.app.ws.ui.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.maneletorres.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.maneletorres.app.ws.ui.model.response.UserRest;
 
 @RestController
@@ -35,9 +40,15 @@ public class UserController {
 		return new ResponseEntity<>(returnValue, HttpStatus.OK);
 	}
 
-	@PostMapping
-	public String createUser() {
-		return "create user was called";
+	@PostMapping(consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+			MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequestModel userDetails) {
+		UserRest returnValue = new UserRest();
+		returnValue.setEmail(userDetails.getEmail());
+		returnValue.setFirstName(userDetails.getFirstName());
+		returnValue.setLastName(userDetails.getLastName());
+
+		return new ResponseEntity<>(returnValue, HttpStatus.OK);
 	}
 
 	@PutMapping
