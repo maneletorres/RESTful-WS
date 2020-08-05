0c7b861a41f4ad19e4de43f11533e88ae34129fa
maneletorres <maneletorres@hotmail.com>
2020-08-05 10:25:45 +0200

    HTTP GET requests handled.

--- src/main/java/com/maneletorres/app/ws/ui/controller/UserController.java
+++ src/main/java/com/maneletorres/app/ws/ui/controller/UserController.java
@@ -2,18 +2,27 @@ package com.maneletorres.app.ws.ui.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("users") // http://localhost:8080/users/
+@RequestMapping("/users") // http://localhost:8080/users/
 public class UserController {
 
 	@GetMapping
-	public String getUser() {
-		return "get user was called";
+	public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
+			@RequestParam(value = "limit", defaultValue = "50") int limit,
+			@RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
+		return "get user was called with page = " + page + " and limit = " + limit + " and sort = " + sort;
+	}
+
+	@GetMapping(path = "/{userId}")
+	public String getUser(@PathVariable String userId) {
+		return "get user was called with userId = " + userId;
 	}
 
 	@PostMapping
